ea10df2684cf5a766eb40ca9ccc1c7d69ac372be
O'Connell, Paul <Paul.O'Connell@mastercard.com>
2020-03-10 11:40:00 +0000

    Add support to handle multiple nifi flow branches.

--- src/main/java/com/github/hermannpencole/nifi/config/NifiApiClient.java
+++ src/main/java/com/github/hermannpencole/nifi/config/NifiApiClient.java
@@ -6,6 +6,9 @@ import com.github.hermannpencole.nifi.config.service.InformationService;
 import com.github.hermannpencole.nifi.config.service.ProcessGroupService;
 import com.github.hermannpencole.nifi.swagger.ApiException;
 import com.github.hermannpencole.nifi.swagger.client.model.ProcessGroupFlowEntity;
+
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +17,8 @@ import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScans({
-  @ComponentScan("com.github.hermannpencole.nifi.config"),
-  @ComponentScan("com.github.hermannpencole.nifi.swagger")
-})
+@ComponentScans({ @ComponentScan("com.github.hermannpencole.nifi.config"),
+@ComponentScan("com.github.hermannpencole.nifi.swagger") })
 public class NifiApiClient {
 
     private final static Logger LOG = LoggerFactory.getLogger(NifiApiClient.class);
@@ -35,12 +36,18 @@ public class NifiApiClient {
     private ProcessGroupService processGroupService;
 
     public void connect() throws ApiException {
-        LOG.info("URL "+properties.url);
-        accessService.setConfiguration(properties.url,
-                properties.verifySsl,
-                properties.debugMode,
-                properties.timeout);
-        accessService.addTokenOnConfiguration(false,properties.username, properties.password);
+        LOG.info("URL " + properties.url);
+        accessService.setConfiguration(properties.url, properties.verifySsl, properties.debugMode, properties.timeout);
+        accessService.addTokenOnConfiguration(false, properties.username, properties.password);
+        String nifiVersion = infoService.getVersion();
+
+        LOG.info(String.format("Starting config_nifi %s on mode %s", nifiVersion, properties.command));
+    }
+
+    public void connect(String URL,String uName,String password,int timeout) throws ApiException {
+        LOG.info("URL " + properties.url);
+        accessService.setConfiguration(URL, properties.verifySsl, properties.debugMode, timeout);
+        accessService.addTokenOnConfiguration(false, uName, password);
         String nifiVersion = infoService.getVersion();
 
         LOG.info(String.format("Starting config_nifi %s on mode %s", nifiVersion, properties.command));
@@ -51,19 +58,36 @@ public class NifiApiClient {
     }
 
     public ProcessGroupFlowEntity getProcessGroupFlow() throws ConfigException, ApiException {
-        LOG.info("loading flow "+properties.branch);
+        LOG.info("loading flow " + properties.branch);
         return processGroupService.createDirectory(properties.getBranchList());
     }
 
+    public ProcessGroupFlowEntity getProcessGroupFlow(List<String> branchFlow) throws ConfigException, ApiException {
+        LOG.info("loading flow " + branchFlow);
+        return processGroupService.createDirectory(branchFlow);
+    }
+
     public void stopProcessGroupFlow() throws ConfigException, ApiException {
-        LOG.info("stop flow "+properties.branch);
+        LOG.info("stop flow " + properties.branch);
         ProcessGroupFlowEntity processGroupFlowEntity = processGroupService.createDirectory(properties.getBranchList());
         processGroupService.stop(processGroupFlowEntity);
     }
 
+    public void stopProcessGroupFlow(List<String> branchFlow) throws ConfigException, ApiException {
+        LOG.info("stop flow " + branchFlow);
+        ProcessGroupFlowEntity processGroupFlowEntity = processGroupService.createDirectory(branchFlow);
+        processGroupService.stop(processGroupFlowEntity);
+    }
+
     public void startProcessGroupFlow() throws ConfigException, ApiException {
-        LOG.info("start flow "+properties.branch);
+        LOG.info("start flow " + properties.branch);
         ProcessGroupFlowEntity processGroupFlowEntity = processGroupService.createDirectory(properties.getBranchList());
+        processGroupService.start(processGroupFlowEntity);
+    }
+
+    public void startProcessGroupFlow(List<String> branchFlow) throws ConfigException, ApiException {
+        LOG.info("start flow " + branchFlow);
+        ProcessGroupFlowEntity processGroupFlowEntity = processGroupService.createDirectory(branchFlow);
         processGroupService.start(processGroupFlowEntity);
     }
 }
